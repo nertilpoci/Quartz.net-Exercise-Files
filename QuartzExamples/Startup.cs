1a5b40f69e190a1f4b4cea69631134f3ddf485c3
Nertil Poci <nertilpoci@hotmail.com>
2018-12-27 17:07:45 +0100

    create

---
+++ QuartzExamples/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Quartz.Impl;
+
+namespace QuartzExamples
+{
+    public class Startup
+    {
+        private IScheduler _quartzScheduler;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _quartzScheduler = ConfigureQuartz();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddSingleton(provider => _quartzScheduler);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+        //this code is called when the application stops
+        private void OnShutdown()
+        {
+            //shutdown quartz is not shutdown already
+            if (!_quartzScheduler.IsShutdown) _quartzScheduler.Shutdown();
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public IScheduler ConfigureQuartz()
+        {
+
+            NameValueCollection props = new NameValueCollection
+             {
+              { "quartz.serializer.type", "binary" },
+              };
+            StdSchedulerFactory factory = new StdSchedulerFactory(props);
+            var scheduler = factory.GetScheduler().Result;
+            scheduler.Start().Wait();
+            return scheduler;
+
+        }
+    }
+}
