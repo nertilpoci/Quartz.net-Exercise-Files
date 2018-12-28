ed602e0cf3b0ae6ea1fde5c5455691e92e033c91
Nertil Poci <nertilpoci@hotmail.com>
2018-12-28 01:05:31 +0100

    part two state

---
+++ QuartzExamples/Jobs/SimpleJob.cs
@@ -0,0 +1,18 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuartzExamples.Jobs
+{
+    public class SimpleJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var message = $"Simple executed at ${DateTime.Now.ToString()}";
+            Debug.WriteLine(message);
+        }
+    }
+}
